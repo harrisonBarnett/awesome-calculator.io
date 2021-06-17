64b5f9a029d4fc78379c9fa72eb5350b8fe87b97
Harrison <h.alexander.barnett@gmail.com>
2021-06-17 02:01:26 -0400

    More functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // getting the innertext of the display
 // this method returns display as an array, hence [0]
-var display = String(document.getElementsByClassName('display')[0].innerText);
+var display = String(document.getElementById('display').innerText);
 
-// getting values from buttons clicked
+// number button event listener
 document.addEventListener('click', function(e) { // bubble method
     if(e.target.className == 'number') {
         display += e.target.value;
@@ -10,11 +10,48 @@ document.addEventListener('click', function(e) { // bubble method
     }
 });
 
-// clearing the screan
+// clearing the screen on button press
 document.getElementById('clear').addEventListener('click', event => {
     display = "";
-    document.getElementsByClassName('display')[0].innerText = "";
+    document.getElementById('display').innerText = "";
 });
+
+
+var operandBefore = "";
+var operator = "";
+var operandAfter = "";
+
+// operator button event listener
+document.addEventListener('click', function(e) { // bubble method
+    if(e.target.className == 'operator') {
+        operandBefore = display;
+        operator = e.target.value;
+        display = ""; // clear between button presses
+        document.getElementById('display').innerText = "";
+    
+    }
+});
+
+// clear cache
+function clear() {
+    operandBefore = "";
+    operator = "";
+    operandAfter = "";
+}
+
+// performing the operation 
+document.getElementById('equals').addEventListener('click', function() {
+    operandAfter = display;
+    solution = "";
+    switch(operator) {
+        case '+':
+            solution = String(add(parseInt(operandBefore), parseInt(operandAfter)));
+            break;
+    }
+    console.log(solution);
+    display = solution;
+});
+
 
 // operations
 function add(a, b) {
