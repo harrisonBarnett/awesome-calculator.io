c2d7bf42ff7093312b6b72e4cf17f7c0ed94c1de
Harrison <h.alexander.barnett@gmail.com>
2021-06-17 00:35:47 -0400

    Some js functionality to pull values from buttons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// getting values from buttons clicked
+document.addEventListener('click', function(e) { // bubble method
+    if(e.target.className == 'number') {
+        console.log(e.target.value);
+    }
+});
+// getting the innertext of the display
+// this method returns display as an array, hence [0]
+const display = document.getElementsByClassName('display')[0].innerText;
+console.log(display);
+
+// operations
 function add(a, b) {
     return a + b;
 }
